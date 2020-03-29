8975d2637be19b4c848c73c2ccffe4d1ac24e4c1
SergiodelaMata <sergio.matam@edu.uah.es>
2020-03-29 23:38:49 +0200

    Added new functions to verify rows, to add the points from each step and to get the list of free spaces at the game field.

--- src/Game.scala
+++ src/Game.scala
@@ -21,6 +21,7 @@ object Game{
     println(colores(4))
     println(colores(5))
     println(random(0,9))*/
+    println(getPosicionesLibres(tablero, 0))
     val posInicial = pedirPosInicial(tablero)
     val posFinal = pedirPosFinal(tablero)
     //println(getValueListOfLists(1,1, tablero))
@@ -100,8 +101,12 @@ object Game{
   
   //Rellena una posición del tablero de forma aleatoria
   def rellenarPosicionAleatorio(tablero: List[List[String]], color : String): List[List[String]] = {
-    val posX = random(0,9)
-    val posY = random(0,9)
+    val listaPosicionesVacias = getPosicionesLibres(tablero, 0)
+    val posicion = listaPosicionesVacias(random(0, listaPosicionesVacias.size))
+    val posX = posicion(0)
+    val posY = posicion(1)
+    //val posX = random(0,9)
+    //val posY = random(0,9)
     if(comprobarIsEmptyPos(posX, posY, tablero)) paintTablero(posX, posY, tablero, color)
     else rellenarPosicionAleatorio(tablero, color)
   }
@@ -147,5 +152,103 @@ object Game{
   def isEqual(value1: String, value2: String): Boolean = {
     if(value1.!=(value2)) false
     else true
+  }
+  
+  //Devuelve el tablero que se va a utilizar para el siguiente paso del juego
+  def getTableroNextPaso(tableroInicial: List[List[String]], tableroFinal: List[List[String]], colores: List[String]): List[List[String]] = {
+    if(getCambiosTablero(tableroInicial, tableroFinal, 0).!=(2)) tableroFinal
+    else rellenarTablero(3, tableroFinal, colores)
+  }
+  
+  //Realiza la suma de puntos del nuevo paso
+  def sumarPuntos(tableroInicial:List[List[String]], tableroFinal:List[List[String]], puntos: Int):Int ={
+    val bolasEliminadas=getCambiosTablero(tableroInicial,tableroFinal,0)
+    if(bolasEliminadas >= 5) puntos + bolasEliminadas*75
+  	else puntos
+  }
+  
+  //Realiza los cambios necesarios en una fila o no dependiendo del número de bolas seguidas de un color
+  def modificarFila(posicion: Int, fila: List[String]): List[String] = {
+    if(getBolasFila(posicion, fila) >= 5)limpiarFila(posicion, fila)
+    else fila
+  }
+  
+  //Realiza la limpieza de las bolas seguidas de un mismo color de una fila a partir de una posición
+  def limpiarFila(posicion: Int, fila: List[String]): List[String] = {
+    posicion match {
+      case 0 => limpiarFilaIzqDer(posicion + 1, fila, getValueList(posicion, fila))
+      case 8 => limpiarFilaDerIzq(posicion - 1, fila, getValueList(posicion, fila))
+      case _ => limpiarFilaIzqDer(posicion + 1, limpiarFilaDerIzq(posicion - 1, fila, getValueList(posicion, fila)), getValueList(posicion, fila))
+    }
+  }
+
+  //Realiza la limpieza de las bolas seguidas de un mismo color de una fila de izquierda a derecha 
+  def limpiarFilaIzqDer(posicion:Int, fila: List[String], color: String): List[String] = {
+    if(posicion < 9)
+    {
+      if(getValueList(posicion,fila).!=(color)) fila
+      else limpiarFilaIzqDer(posicion + 1, paintFila(posicion, fila, null), color)
+    }
+    else fila
+  }
+  
+  //Realiza la limpieza de las bolas seguidas de un mismo color de una fila de derecha a izquierda
+  def limpiarFilaDerIzq(posicion:Int, fila: List[String], color: String): List[String] = {
+    if(posicion >= 0)
+    {
+      if(getValueList(posicion,fila).!=(color)) fila
+      else limpiarFilaDerIzq(posicion - 1, paintFila(posicion, fila, null), color)
+    }
+    else fila
+  }
+  
+  //Devuelve el número de bolas seguidas de un mismo color a partir de una posición en una fila
+  def getBolasFila(posicion: Int, fila: List[String]): Int = {
+    posicion match {
+      case 0 => 1 + getBolasFilaIzqDer(posicion + 1, fila, getValueList(posicion, fila))
+      case 8 => 1 + getBolasFilaDerIzq(posicion - 1, fila, getValueList(posicion, fila))
+      case _ => getBolasFilaIzqDer(posicion + 1, fila, getValueList(posicion, fila)) + 1 + getBolasFilaDerIzq(posicion - 1, fila, getValueList(posicion, fila))
+    }
+  }
+  
+  //Devuelve el número de bolas seguidas de un mismo color de izquierda a derecha en una fila
+  def getBolasFilaIzqDer(posicion: Int, fila: List[String], color: String): Int = {
+    if(posicion < 9) //No supera el margen de la derecha
+    {
+      //Comprobar si tienen el mismo color
+      if(getValueList(posicion, fila).!=(color)) 0
+      else 1 + getBolasFilaIzqDer(posicion + 1, fila, color)
+    }
+    else 0 //Fuera de rango
+  }
+  
+  //Devuelve el número de bolas seguidas de un mismo color de derecha a izquierda en una fila
+  def getBolasFilaDerIzq(posicion: Int, fila: List[String], color: String): Int = {
+    if(posicion >= 0) //No supera el margen de la izquierda
+    {
+      //Comprobar si tienen el mismo color
+      if(getValueList(posicion, fila).!=(color)) 0
+      else 1 + getBolasFilaIzqDer(posicion - 1, fila, color)
+    }
+    else 0 //Fuera de rango
+  }
+  
+  //Ajusta los nuevos valores para una fila del tablero
+  def setFila(posicion: Int, fila: List[String], tablero: List[List[String]]): List[List[String]] = {
+    if(posicion.!=(0)) tablero.head :: setFila(posicion - 1, fila, tablero.tail)
+    else fila :: tablero.tail
+  }
+  
+  def getPosicionesLibres(tablero: List[List[String]], posicion: Int): List[List[Int]] = {
+    val posX = posicion%9
+    val posY = posicion/9
+    println(List(List(posX, posY)))
+    println(posicion)
+    if(posicion >= 81) List(List())
+    else
+    {
+      if(comprobarIsEmptyPos(posX, posY, tablero)) List(List(posX, posY)) ::: getPosicionesLibres(tablero, posicion + 1)
+      else getPosicionesLibres(tablero, posicion + 1)
+    }
   }
 }
